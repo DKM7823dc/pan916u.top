bc28db4ebb2ffd3ec7ed014ae310c81780c9ea2e
DKM7823dc <mkyusds82a@hotmail.com>
2025-11-21 22:00:08 +0800

    Add files via upload

---
+++ sac/js/config.js
@@ -0,0 +1,307 @@
+﻿window.siteConfig = {
+    "banner_settings": {
+        "enabled": true,
+        "keywords1111": [
+            "\u4ece\u672a\u89c1\u8fc7\u306e\u52a8\u6f2b",
+            "\u81ea\u7531\u53d1\u6325",
+            "\u81ea\u7531\u53d1\u6325",
+            "\u56de\u5bb6\u7684\u8def",
+            "\u90fd\u662f\u54e5\u4eec",
+            "\u5e94\u6709\u5c3d\u6709"
+        ],
+        "domain": "Pix.hair",
+        "positions": [
+            {
+                "top": "25%",
+                "left": "13%"
+            },purchase-link
+            {
+                "top": "37%",
+                "left": "13%"
+            },
+            {
+                "top": "49%",
+                "left": "13%"
+            },
+            {
+                "top": "61%",
+                "left": "13%"
+            },
+            {
+                "top": "73%",
+                "left": "13%"
+            },
+            {
+                "top": "85%",
+                "left": "13%"
+            }
+        ],
+        "text_color": "#000000",
+        "text_size": "12px",
+        "font_weight": "bold",
+        "positions_with_image": [
+            {
+                "top": "51%",
+                "left": "13%"
+            },
+            {
+                "top": "59%",
+                "left": "13%"
+            },
+            {
+                "top": "67%",
+                "left": "13%"
+            },
+            {
+                "top": "75%",
+                "left": "13%"
+            },
+            {
+                "top": "83%",
+                "left": "13%"
+            },
+            {
+                "top": "90%",
+                "left": "13%"
+            }
+        ]
+    },
+    "keywords": [
+        {
+            "text": "\u706b\u5f71\u540c\u4eba",
+            "weight": 20,
+            "fixed": true,
+            "color": "#ff0000"
+        },
+        {
+            "text": "\u7279\u5de517",
+            "weight": 20,
+            "fixed": false
+        },
+        {
+            "text": "\u706b\u5f71\u8fd9\u4e00\u5757",
+            "weight": 20,
+            "fixed": false,
+            "color": "#ff0000"
+        },
+        {
+            "text": "\u672c\u5b50\u63a8\u8350",
+            "weight": 20,
+            "fixed": false,
+            "color": "#ff0000"
+        },
+        {
+            "text": "\u9ec4\u6cb9",
+            "weight": 20,
+            "fixed": false
+        },
+        {
+            "text": "\u67ef\u5357\u540c\u4eba",
+            "weight": 20,
+            "fixed": false,
+            "color": "#ff0000"
+        },
+        {
+            "text": "jm3",
+            "weight": 20,
+            "fixed": false,
+            "color": "#ff0000"
+        },
+        {
+            "text": "\u6700\u65b0\u540c\u4eba",
+            "weight": 20,
+            "fixed": false
+        },
+        {
+            "text": "\u56fd\u6f2b\u5973\u795e",
+            "weight": 20,
+            "fixed": false
+        },
+        {
+            "text": "\u706b\u5f71\u5fcd\u8005\u672c",
+            "weight": 20,
+            "fixed": false,
+            "color": "#ff0000"
+        },
+        {
+            "text": "\u6f2b\u753b\u63a8\u8350",
+            "weight": 20,
+            "fixed": false
+        },
+        {
+            "text": "\u539f\u795e\u540c\u4eba",
+            "weight": 20,
+            "fixed": false
+        }
+    ],
+    "keyword_settings": {
+        "min_keywords": 3,
+        "max_keywords": 4,
+        "default_color": "#1798fc",
+        "line_break_after": 2
+    },
+    "comments": [
+        {
+            "text": "\u7528\u4e24\u4e09\u5e74\u4e86\u90fdPix.hair",
+            "weight": 20,
+            "fixed": true
+        },
+        {
+            "text": "Cctv.surf\u7528\u4e09\u56db\u5e74\u4e86\u90fd",
+            "weight": 20,
+            "fixed": false
+        },
+        {
+            "text": "\u6211\u8fd9\u662f\u6700\u597d\u7684Yxz.pink",
+            "weight": 20,
+            "fixed": false
+        },
+        {
+            "text": "\u6211\u8fd9\u662f\u6700\u597d\u7684Dvd.hair",
+            "weight": 20,
+            "fixed": false
+        },
+        {
+            "text": "\u6211\u8fd9\u662f\u6700\u597d\u7684Vcd.asia",
+            "weight": 20,
+            "fixed": false
+        }
+    ],
+    "comment_settings": {
+        "display_count": 3,
+        "domain": "",
+        "repeat_display": 1,
+        "repeat_copy": 1,
+        "separator": "<br>",
+        "default_color": "#1798fc",
+        "domain_positions": [
+            "after",
+            "after",
+            "after",
+            "after"
+        ],
+        "copy_as_text": false,
+        "copy_separator": "\\n",
+        "random_domains": [
+            "exc.pics",
+            "vyy.life",
+            "gke.asia",
+            "985.buzz",
+            "798.wtf"
+        ],
+        "random_domain_count": 1,
+        "enable_random_domains": false,
+        "random_domain_position": true,
+        "same_domain_per_refresh": false
+    },
+    "emoji_settings": {
+        "enabled": false,
+        "pool": [
+            "[\u5e86\u795d]",
+            "[\u5e72\u996d\u4eba]",
+            "[\u563f\u54c8]",
+            "[\u5403\u74dc\u7fa4\u4f17]",
+            "[\u6c42\u673a\u4f4d-\u9ec4\u8138]",
+            "[\u6c42\u673a\u4f4d3]",
+            "[\u70b9\u8d5e]",
+            "[\u7cbe\u9009]",
+            "[okk]",
+            "[V5]",
+            "[\u7edd]",
+            "[\u7ed9\u529b]",
+            "[18\u7981]"
+        ],
+        "positions": [
+            "before",
+            "before",
+            "before",
+            "after"
+        ],
+        "random_position": true,
+        "repeat_with_comment": false,
+        "show_in_display": false
+    },
+    "image_settings": {
+        "show_example_image": false
+    },
+    "site_redirect": {
+        "enabled": false,
+        "url": "https:\/\/xt9.lol\/",
+        "delay": 0
+    },
+    "step_texts": {
+        "search_instruction": "\u6253\u5f00\u6296\u97f3\/\u5feb\u624b\/B\u7ad9\u641c\u7d22\u4e0b\u9762\u5176\u4e2d\u4e00\u4e2a\u5173\u952e\u8bcd\u3010\u70b9\u51fb\u590d\u5236\u3011\u2193\u2193",
+        "search_instruction_jump": "\u70b9\u51fb\u4e0b\u9762\u5176\u4e2d\u4e00\u4e2a\u5173\u952e\u8bcd\u8fdb\u5165\u6296\u97f3 \u2193\u2193",
+        "search_instruction_font_weight": "bold",
+        "alternative_search": "\u6216\u8005\u53bb\u641c\u4f60\u60f3\u770b\u7684\u52a8\u6f2b\u6216\u672c\u5b50\u6216\u6e38\u620f\u540d\u5b57\uff0c\u53ea\u8981\u76f8\u5173\u7684\u89c6\u9891\u90fd\u53ef\u4ee5\u8bc4\u8bba",
+        "alternative_search_color": "#e03e2d",
+        "alternative_search_font_weight": "bold",
+        "alternative_search_jump": "\u6216\u8005\u53bb\u6296\u97f3\u641c\u7d22\u5176\u4ed6\u4f60\u60f3\u770b\u7684\u5185\u5bb9",
+        "alternative_search_jump_color": "#e03e2d",
+        "alternative_search_jump_font_weight": "bold"
+    },
+    "keyword_links": {
+        "enabled": false,
+        "items": []
+    },
+    "purchase_link": "https:\/\/fkfk.fafafa8.cn\/links\/76F1E90C",
+    "game_links": {
+        "intro_text": "\u2705 \u5b9d\u5b50\u606d\u559c\u4f60\u83b7\u5f97\u8d44\u6e90\uff08\u4e00\u5171\u67094\u4e2a\u83b7\u53d6\u5165\u53e3\uff0c\u5982\u679c\u5176\u4e2d\u4e00\u4e2a\u548c\u8c10\u6216\u8005\u6ca1\u6709\u60f3\u770b\u7684\u5c31\u6362\u4e00\u4e2a\u6e20\u9053\uff09\u901f\u5ea6\u4fdd\u5b58\u5230\u81ea\u5df1\u7f51\u76d8\uff0c\u56e0\u4e3a\u94fe\u63a5\u968f\u65f6\u5931\u6548\uff0c4\u4e2a\u6e20\u9053\u5efa\u8bae\u5168\u90e8\u4fdd\u5b58\u5230\u81ea\u5df1\u7684\u7f51\u76d8\uff0c\u56e0\u4e3a\u6bcf\u4e2a\u7f51\u76d8\u7684\u8d44\u6e90\u90fd\u4e0d\u4e00\u6837",
+        "links": [
+            {
+                "url": "https:\/\/pan.quark.cn\/s\/7214ff9e4398",
+                "title": "\u6e20\u90531 \u2192\u5938\u514b\u7f51\u76d8800G \u706b\u5f71\/\u67ef\u5357\/\u56fd\u6f2b\u5973\u795e\/JM\/\u7279\u5de5\/\u540c\u4eba\u52a8\u6f2b\u5e94\u6709\u5c3d\u6709",
+                "icon": "\ud83d\udd1e",
+                "enabled": true
+            },
+            {
+                "url": "https:\/\/drive.uc.cn\/s\/40e17194f08d4?public=1",
+                "title": "\u6e20\u90532 \u2192UC\u7f51\u76d8600G \u6700\u65b0\u70ed\u74dc\/\u7f51\u7ea2\u9ed1\u6599\/JM\u8f6f\u4ef6\/\u5e94\u6709\u5c3d\u6709",
+                "icon": "\ud83d\udd1e",
+                "enabled": true
+            },
+            {
+                "url": "http:\/\/839.1.kk.qqapi.cc.cdn.cloudflare.net\/reg-v1\/q\/11\/?yqr=321&bj=&url=https%3A%2F%2Fdrive.uc.cn%2Fs%2F40e17194f08d4%3Fpublic%3D1&smsType=1&wp=",
+                "title": "\u5f3a\u70c8\u63a8\u8350\u2192\u70ed\u95e8H\u6e38\/\u67ef\u5357\/\u706b\u5f71\u540c\u4eba\u5e94\u6709\u5c3d\u6709\u300a\u5982\u679c\u5931\u6548\u8bf7\u79fb\u6b65\u5230\u6e20\u90531\u62162\u300b",
+                "icon": "\ud83d\udd1e",
+                "enabled": true
+            },
+            {
+                "url": "https:\/\/qm.qq.com\/q\/55hbjjWX0s",
+                "title": "\u70b9\u51fb\u52a0\u5165\u52a8\u6f2b\u5929\u5802\u7b49\u901a\u77e5~",
+                "icon": "\ud83d\udd1e",
+                "enabled": true
+            }
+        ]
+    },
+    "layout_settings": {
+        "step1_keywords": "left",
+        "step2_comments": "center"
+    }
+};
+window.siteRedirect = {"enabled":false,"url":"https:\/\/xt9.lol\/","delay":0};
+window.purchaseLink = 'https://fkfk.fafafa8.cn/links/76F1E90C';
+window.stepTexts = {"search_instruction":"\u6253\u5f00\u6296\u97f3\/\u5feb\u624b\/B\u7ad9\u641c\u7d22\u4e0b\u9762\u5176\u4e2d\u4e00\u4e2a\u5173\u952e\u8bcd\u3010\u70b9\u51fb\u590d\u5236\u3011\u2193\u2193","search_instruction_jump":"\u70b9\u51fb\u4e0b\u9762\u5176\u4e2d\u4e00\u4e2a\u5173\u952e\u8bcd\u8fdb\u5165\u6296\u97f3 \u2193\u2193","search_instruction_font_weight":"bold","alternative_search":"\u6216\u8005\u53bb\u641c\u4f60\u60f3\u770b\u7684\u52a8\u6f2b\u6216\u672c\u5b50\u6216\u6e38\u620f\u540d\u5b57\uff0c\u53ea\u8981\u76f8\u5173\u7684\u89c6\u9891\u90fd\u53ef\u4ee5\u8bc4\u8bba","alternative_search_color":"#e03e2d","alternative_search_font_weight":"bold","alternative_search_jump":"\u6216\u8005\u53bb\u6296\u97f3\u641c\u7d22\u5176\u4ed6\u4f60\u60f3\u770b\u7684\u5185\u5bb9","alternative_search_jump_color":"#e03e2d","alternative_search_jump_font_weight":"bold"};
+window.keywordLinks = {"enabled":false,"items":[]};
+window.imageSettings = {"show_example_image":false};
+window.keywordPool = [{"text":"\u706b\u5f71\u540c\u4eba","weight":20,"fixed":true,"color":"#ff0000"},{"text":"\u7279\u5de517","weight":20,"fixed":false},{"text":"\u706b\u5f71\u8fd9\u4e00\u5757","weight":20,"fixed":false,"color":"#ff0000"},{"text":"\u672c\u5b50\u63a8\u8350","weight":20,"fixed":false,"color":"#ff0000"},{"text":"\u9ec4\u6cb9","weight":20,"fixed":false},{"text":"\u67ef\u5357\u540c\u4eba","weight":20,"fixed":false,"color":"#ff0000"},{"text":"jm3","weight":20,"fixed":false,"color":"#ff0000"},{"text":"\u6700\u65b0\u540c\u4eba","weight":20,"fixed":false},{"text":"\u56fd\u6f2b\u5973\u795e","weight":20,"fixed":false},{"text":"\u706b\u5f71\u5fcd\u8005\u672c","weight":20,"fixed":false,"color":"#ff0000"},{"text":"\u6f2b\u753b\u63a8\u8350","weight":20,"fixed":false},{"text":"\u539f\u795e\u540c\u4eba","weight":20,"fixed":false}];
+window.commentPool = [{"text":"\u7528\u4e24\u4e09\u5e74\u4e86\u90fdPix.hair","weight":20,"fixed":true},{"text":"Cctv.surf\u7528\u4e09\u56db\u5e74\u4e86\u90fd","weight":20,"fixed":false},{"text":"\u6211\u8fd9\u662f\u6700\u597d\u7684Yxz.pink","weight":20,"fixed":false},{"text":"\u6211\u8fd9\u662f\u6700\u597d\u7684Dvd.hair","weight":20,"fixed":false},{"text":"\u6211\u8fd9\u662f\u6700\u597d\u7684Vcd.asia","weight":20,"fixed":false}];
+window.keywordSettings = {"min_keywords":3,"max_keywords":4,"default_color":"#1798fc","line_break_after":2};
+window.commentSettings = {"display_count":3,"domain":"","repeat_display":1,"repeat_copy":1,"separator":"<br>","default_color":"#1798fc","domain_positions":["after","after","after","after"],"copy_as_text":false,"copy_separator":"\\n","random_domains":["exc.pics","vyy.life","gke.asia","985.buzz","798.wtf"],"random_domain_count":1,"enable_random_domains":false,"random_domain_position":true,"same_domain_per_refresh":false};
+window.bannerSettings = {"enabled":true,"keywords":["\u4ece\u672a\u89c1\u8fc7\u306e\u52a8\u6f2b","\u81ea\u7531\u53d1\u6325","\u81ea\u7531\u53d1\u6325","\u56de\u5bb6\u7684\u8def","\u90fd\u662f\u54e5\u4eec","\u5e94\u6709\u5c3d\u6709"],"domain":"Pix.hair","positions":[{"top":"25%","left":"13%"},{"top":"37%","left":"13%"},{"top":"49%","left":"13%"},{"top":"61%","left":"13%"},{"top":"73%","left":"13%"},{"top":"85%","left":"13%"}],"text_color":"#000000","text_size":"12px","font_weight":"bold","positions_with_image":[{"top":"51%","left":"13%"},{"top":"59%","left":"13%"},{"top":"67%","left":"13%"},{"top":"75%","left":"13%"},{"top":"83%","left":"13%"},{"top":"90%","left":"13%"}]};
+window.emojiSettings = {"enabled":false,"pool":["[\u5e86\u795d]","[\u5e72\u996d\u4eba]","[\u563f\u54c8]","[\u5403\u74dc\u7fa4\u4f17]","[\u6c42\u673a\u4f4d-\u9ec4\u8138]","[\u6c42\u673a\u4f4d3]","[\u70b9\u8d5e]","[\u7cbe\u9009]","[okk]","[V5]","[\u7edd]","[\u7ed9\u529b]","[18\u7981]"],"positions":["before","before","before","after"],"random_position":true,"repeat_with_comment":false,"show_in_display":false};
+window.gameLinks = {"intro_text":"\u2705 \u5b9d\u5b50\u606d\u559c\u4f60\u83b7\u5f97\u8d44\u6e90\uff08\u4e00\u5171\u67094\u4e2a\u83b7\u53d6\u5165\u53e3\uff0c\u5982\u679c\u5176\u4e2d\u4e00\u4e2a\u548c\u8c10\u6216\u8005\u6ca1\u6709\u60f3\u770b\u7684\u5c31\u6362\u4e00\u4e2a\u6e20\u9053\uff09\u901f\u5ea6\u4fdd\u5b58\u5230\u81ea\u5df1\u7f51\u76d8\uff0c\u56e0\u4e3a\u94fe\u63a5\u968f\u65f6\u5931\u6548\uff0c4\u4e2a\u6e20\u9053\u5efa\u8bae\u5168\u90e8\u4fdd\u5b58\u5230\u81ea\u5df1\u7684\u7f51\u76d8\uff0c\u56e0\u4e3a\u6bcf\u4e2a\u7f51\u76d8\u7684\u8d44\u6e90\u90fd\u4e0d\u4e00\u6837","links":[{"url":"https:\/\/pan.quark.cn\/s\/7214ff9e4398","title":"\u6e20\u90531 \u2192\u5938\u514b\u7f51\u76d8800G \u706b\u5f71\/\u67ef\u5357\/\u56fd\u6f2b\u5973\u795e\/JM\/\u7279\u5de5\/\u540c\u4eba\u52a8\u6f2b\u5e94\u6709\u5c3d\u6709","icon":"\ud83d\udd1e","enabled":true},{"url":"https:\/\/drive.uc.cn\/s\/40e17194f08d4?public=1","title":"\u6e20\u90532 \u2192UC\u7f51\u76d8600G \u6700\u65b0\u70ed\u74dc\/\u7f51\u7ea2\u9ed1\u6599\/JM\u8f6f\u4ef6\/\u5e94\u6709\u5c3d\u6709","icon":"\ud83d\udd1e","enabled":true},{"url":"http:\/\/839.1.kk.qqapi.cc.cdn.cloudflare.net\/reg-v1\/q\/11\/?yqr=321&bj=&url=https%3A%2F%2Fdrive.uc.cn%2Fs%2F40e17194f08d4%3Fpublic%3D1&smsType=1&wp=","title":"\u5f3a\u70c8\u63a8\u8350\u2192\u70ed\u95e8H\u6e38\/\u67ef\u5357\/\u706b\u5f71\u540c\u4eba\u5e94\u6709\u5c3d\u6709\u300a\u5982\u679c\u5931\u6548\u8bf7\u79fb\u6b65\u5230\u6e20\u90531\u62162\u300b","icon":"\ud83d\udd1e","enabled":true},{"url":"https:\/\/qm.qq.com\/q\/55hbjjWX0s","title":"\u70b9\u51fb\u52a0\u5165\u52a8\u6f2b\u5929\u5802\u7b49\u901a\u77e5~","icon":"\ud83d\udd1e","enabled":true}]};
+window.layoutSettings = {"step1_keywords":"left","step2_comments":"center"};
+let currentSessionDomain = null;
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const purchaseLink = document.getElementById('purchase-link');
+//     if (purchaseLink && window.purchaseLink) {
+//         purchaseLink.href = window.purchaseLink;
+//     }
+//     initSiteRedirect();
+//     initStepTexts();
+//     initBannerKeywords(); 
+//     setKeywordLayout();
+//     toggleExampleImage(); 
+// });
